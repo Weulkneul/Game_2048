66b3d9aed6ea740d667a5b19614dc06fc089a636
Weulkneul <schroej@hochschule-trier.de>
2016-09-13 15:09:37 +0200

    Initialize() überarbeitet

--- field.js
+++ field.js
@@ -1,44 +1,79 @@
+'use strict';
 class field {
+
    constructor(width, matrix){
        this.width = width;
        this.matrix = matrix;
    }
+
    initialize() {
-    let randnum = {
-        calculate(min, max){
-       return Math.floor(Math.random()*(max-min+1))+min;
-        }
-    }
     this.matrix =  [[0, 0, 0, 0],
                     [0, 0, 0, 0],
                     [0, 0, 0, 0],
                     [0, 0, 0, 0]];
-    let probability = function(){
-        let num = Math.random();
-        if(num < 0.9){
-            return 2;
-        }
-        else {
-            return 4;
+    let randnum = {
+        calculate(min, max){
+            return Math.floor(Math.random()*(max-min+1))+min;
         }
     }
-    let zahl1 = randnum.calculate(1,16);
-    this.matrix[zahl1/4][zahl1%4] = probability;
+    let probability = {
+        getNum(){
+            let num = Math.random();
+            if(num < 0.9){
+                return 2;
+            }
+            else {
+                return 4;
+            }
+        }
+    }
+    let index1 = {
+        getIndex(wert) {
+            return Math.ceil(wert / 4) - 1;
+        }
+    }
+    let index2 = {
+        getIndex(wert){
+            if(wert % 4 == 0) {
+                return 3;
+            }
+            else {
+                return wert % 4 -1;
+            }
+        }
+    }
+    var zahl1 = randnum.calculate(1,16);
+    console.log(zahl1);
+    this.matrix[index1.getIndex(zahl1)][index2.getIndex(zahl1)] = probability.getNum();
     do{
-        let zahl2 = randnum.calculate(1,16);
+        var zahl2 = randnum.calculate(1,16);
     } while(zahl1 === zahl2)
-    this.matrix[zahl2/4][zahl2%4] = probability;
+    console.log(zahl2);
+    this.matrix[index1.getIndex(zahl2)][index2.getIndex(zahl2)] = probability.getNum();
    }
+
    right(){
 
    }
+
    left(){
 
    }
+
    up(){
 
    }
+
    down(){
        
    }
+
+   play(){
+    console.log(this.matrix);
+   }
+
 }
+
+var game = new field(80, null);
+game.initialize();
+game.play();
